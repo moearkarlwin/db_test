5e1a39ee56ce291c5f398ae48a2ab94bd8dd3f22
moearkarlwin <moearkarlwin@gmail.com>
2025-05-08 00:38:05 +0630

    DB Connection Commit

--- src/main/java/com/dbproject/Main.java
+++ src/main/java/com/dbproject/Main.java
@@ -1,8 +1,24 @@
 package com.dbproject;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
 
+    private Connection db_connect() {
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb", "root", "");
+            System.out.println("Successful connected.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Main m = new Main();
+        Connection con = m.db_connect();
     }
 }
